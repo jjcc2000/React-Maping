f050c21aa1b78ef27cbf9e68103daee1992c8740
esJohan <imjohanjjcc@gmail.com>
2023-06-01 17:45:39 -0600

    AAA

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Footer() {
+  const d = new Date().getFullYear();
+  return (
+    <footer>
+      <p>Copyright ⓒ {d}</p>
+    </footer>
+  );
+}
+
+export default Footer;
